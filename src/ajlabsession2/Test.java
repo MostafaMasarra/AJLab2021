fe519039dad7202388345b0b464bf7a82e0653b0
Bader <Bader@172.20.22.57>
2020-10-31 17:59:40 +0200

    Test file to see constructor usage

---
+++ src/ajlabsession2/Test.java
@@ -0,0 +1,28 @@
+package ajlabsession2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    private String textName;
+    
+    //default constructor
+    public Test() {
+        System.out.println("Test constructor is invoked");
+    }
+    
+    //constructor with parameter
+    public Test(String textName){
+        this.textName = textName;
+    }
+    
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        System.out.println("start method is invoked");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("launch application");
+        Application.launch(args);
+    }
+}
