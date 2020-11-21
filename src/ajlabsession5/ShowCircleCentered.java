d71cec9f2a2ee297188b0f5b4f3ebbc5e983c0d5
Bader <Bader@172.20.22.57>
2020-11-21 23:08:40 +0200

    Session 5 - Binding properties Examples

---
+++ src/ajlabsession5/ShowCircleCentered.java
@@ -0,0 +1,42 @@
+package ajlabsession5;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class ShowCircleCentered extends Application {
+  @Override // Override the start method in the Application class
+  public void start(Stage primaryStage) {    
+    // Create a pane to hold the circle 
+    Pane pane = new Pane();
+    
+    // Create a circle and set its properties
+    Circle circle = new Circle();
+    circle.setRadius(50);
+    circle.setStroke(Color.BLACK); 
+    circle.setFill(Color.WHITE);
+    
+    //bind centerX and centerY properties so that the circle remains centered to its pane
+    circle.centerXProperty().bind(pane.widthProperty().divide(2));
+    circle.centerYProperty().bind(pane.heightProperty().divide(2));
+    
+    pane.getChildren().add(circle); // Add circle to the pane
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 200, 200);
+    primaryStage.setTitle("ShowCircleCentered"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+  }
+  
+  /**
+   * The main method is only needed for the IDE with limited
+   * JavaFX support. Not needed for running from the command line.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
