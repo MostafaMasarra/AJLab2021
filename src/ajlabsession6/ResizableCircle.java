68a290205be6bc270a35c53d54499fbdad449766
Bader <Bader@172.20.22.57>
2020-11-26 02:51:29 +0200

    Session 6 - Implementation of Event Handlers using different methods

---
+++ src/ajlabsession6/ResizableCircle.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ajlabsession6;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+
+/**
+ *
+ * @author Bader
+ */
+public class ResizableCircle extends StackPane {
+
+    private final Circle circle;
+
+    ResizableCircle(int radius) {
+        circle = new Circle(radius);
+        this.getChildren().add(circle);
+    }
+
+    void enlarge() {
+        this.circle.setRadius(this.circle.getRadius() * 1.1);
+    }
+
+    void shrink() {
+        this.circle.setRadius(this.circle.getRadius() / 1.1);
+    }
+}
